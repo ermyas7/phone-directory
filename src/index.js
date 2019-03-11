dd4412aa8c0a5b4f40e04e46fa8710ff1444f654
ermyas7 <ermyas.fekadu@gmail.com>
2019-03-11 23:18:22 +0530

    Add contact form

--- src/index.js
+++ src/index.js
@@ -9,6 +9,32 @@ function Header(props){
     );
 }
 
+function ContactForm(props){
+    return(
+        <div className="person">
+          <a href="#" className="person-back">Back</a>  
+          <form className="person-contact">
+            <div className="person-contact-name">
+                <label for="name" className="person-contact-name-label">Name:</label>
+                <input type="text" className="person-contact-name-input" id="name"/>
+            </div>
+            <div className="person-contact-number">
+                <label for="number" className="person-contact-number-label">
+                    Phone:
+                </label>
+                <input type="text" className="person-contact-number-input" id="number"/>
+            </div>
+            <div className="person-value">
+                <p className="person-value-subscribe">Subscriber to be added:</p>
+                <p className="person-value-name">Name:</p>
+                <p className="person-value-number">Phone:</p>
+            </div>
+            <a href="#" class="add">Add</a>
+        </form>  
+        </div>
+    );
+}
+
 class PhoneDirectory extends React.Component{
     render(){
         
